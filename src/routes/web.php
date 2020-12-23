0538976e2d2aeb8755dcf183d11c41f3965f7e74
Wangshen <kongtiaowangshen@gmail.com>
2020-12-23 10:39:03 -0500

    controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,18 +14,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [PagesController::class, 'index']);
+
+
+Route::get('/hello', function () {
+    return view('hello');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);
+
+
+
+Route::get('/users/{id}', function ($id) {
+    return "this is a user".$id;
+});
+
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
